8d2ebb818a6b67dd5e2ee382096e70ecc5c0e641
Shubham <patilshubham41@gmail.com>
2021-05-22 15:27:03 +0530

    First git commit for selenium prj

---
+++ src/main/com/qa/libs/SetUp.java
@@ -0,0 +1,12 @@
+package src.main.com.qa.libs;
+
+public class SetUp {
+
+	
+	public void setUp()
+	{
+		System.out.println("SetUp is running");
+	}
+	
+	
+}
---
+++ src/main/com/qa/pages/IndexPage.java
@@ -0,0 +1,11 @@
+package src.main.com.qa.pages;
+
+public class IndexPage {
+
+	public void indexPage()
+	{
+		System.out.println("Index page is running");
+	}
+	
+	
+}
